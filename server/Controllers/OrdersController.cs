67c88ba9ad9ffd67f4690f33df5dbf2f01fc3f30
Davis <davisdjaja20@gmail.com>
2024-03-30 13:53:53 +1300

    backend checkout feature changes

--- server/Controllers/OrdersController.cs
+++ server/Controllers/OrdersController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -40,7 +36,7 @@ namespace server.Controllers
         {
             var basket = await _context.Baskets.RetrieveBasketWithItems(User.Identity.Name).FirstOrDefaultAsync();
 
-            if(basket == null) return BadRequest(new ProblemDetails{Title = "Could not locate basket"});
+            if (basket == null) return BadRequest(new ProblemDetails { Title = "Could not locate basket" });
 
             var items = new List<OrderItem>();
 
@@ -53,7 +49,8 @@ namespace server.Controllers
                     Name = productItem.Name,
                     PictureUrl = productItem.PictureUrl
                 };
-                var orderItem = new OrderItem{
+                var orderItem = new OrderItem
+                {
                     ItemOrdered = itemOrdered,
                     Price = productItem.Price,
                     Quantity = item.Quantity
@@ -77,10 +74,10 @@ namespace server.Controllers
             _context.Orders.Add(order);
             _context.Baskets.Remove(basket);
 
-            if(orderDto.SaveAddress)
+            if (orderDto.SaveAddress)
             {
-                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
-                user.Address = new UserAddress
+                var user = await _context.Users.Include(a => a.Address).FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+                var address = new UserAddress
                 {
                     FullName = orderDto.ShippingAddress.FullName,
                     Address1 = orderDto.ShippingAddress.Address1,
@@ -90,12 +87,12 @@ namespace server.Controllers
                     Zip = orderDto.ShippingAddress.Zip,
                     Country = orderDto.ShippingAddress.Country,
                 };
-                _context.Update(user);
+                user.Address = address;
             }
 
             var result = await _context.SaveChangesAsync() > 0;
 
-            if (result) return CreatedAtRoute("GetOrder", new {id = order.Id}, order.Id);
+            if (result) return CreatedAtRoute("GetOrder", new { id = order.Id }, order.Id);
 
             return BadRequest("Problem creating order");
         }
